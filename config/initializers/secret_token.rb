25e427a1c54b613b1cf3d04e926f93b51a8541a3
Ismail Hanli <ihanli.mmt-b2009@fh-salzburg.ac.at>
2011-04-19 12:39:57 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PuzzlesBackEnd::Application.config.secret_token = 'b065c8cb429b9fdd9375579f34ccb3961bcd45b0d369c13a659012b1600506669cc96ef7e163520d1045953e4f688cffa1181baa208c28be50020e892c154c8f'
