95ec4a8595f69d1b6d2f5d8d0b450c378eb27c75
Ismail Hanli <ihanli.mmt-b2009@fh-salzburg.ac.at>
2011-04-20 18:07:39 +0200

    added validations to models

--- app/models/spell.rb
+++ app/models/spell.rb
@@ -1,3 +1,6 @@
 class Spell < ActiveRecord::Base
   belongs_to :abstract_card, :dependent => :delete
+  
+  validates_presence_of :abstract_card_id, :duration
+  validates_numericality_of :duration, :only_integer => true, :minimum => -1
 end
--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,3 +1,7 @@
 class Unit < ActiveRecord::Base
   belongs_to :abstract_card, :dependent => :delete
+  
+  validates_presence_of :attack, :health, :abstract_card_id
+  validates_numericality_of :attack, :only_integer => true, :greater_than_or_equal_to => 0
+  validates_numericality_of :health, :only_integer => true, :greater_than_or_equal_to => 0
 end
