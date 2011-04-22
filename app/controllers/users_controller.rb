c7c447c7c0b78b30e21fc6f209946cfac473b00a
Ismail Hanli <ihanli.mmt-b2009@fh-salzburg.ac.at>
2011-04-22 17:00:50 +0200

    generated controller for User model and added some actions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+      
+    respond_to do |format|
+     format.xml
+    end
+  end
+
+  def new
+  end
+  
+  def create
+    @user = User.new(params[:user])
+      
+    if @user.save
+      redirect_to user_path(@user.id)
+    else
+      render :status => 500
+    end
+  end
+  
+  def destroy
+    @user = User.find(params[:id])
+        
+    if @user.destroy
+      redirect_to users_path
+    else
+      render :status => 500
+    end
+  end
+end
