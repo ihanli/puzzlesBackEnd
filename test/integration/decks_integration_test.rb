04d82de730b360db9ea19daab94884eead6477d1
Ismail Hanli <ihanli.mmt-b2009@fh-salzburg.ac.at>
2011-04-24 14:03:57 +0200

    rudimentary integration tests for show and index action of decks_controller

---
+++ test/integration/decks_integration_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class TasksIntegrationTest < ActionDispatch::IntegrationTest
+  should "show all decks of an user" do
+    Deck.make(Deck.plan)
+    visit decks_path
+    assert respond_with(:success)
+  end
+  
+  should "show a single deck" do
+    Deck.make(Deck.plan)
+    visit deck_path(1)
+    assert respond_with(:success)
+  end
+end
