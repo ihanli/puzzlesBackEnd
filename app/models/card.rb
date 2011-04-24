6c4a1c20e942eb5de4be80b0ade5a7b9acae3ff9
Ismail Hanli <ihanli.mmt-b2009@fh-salzburg.ac.at>
2011-04-24 13:59:09 +0200

    added class methods to get records over user id

--- app/models/card.rb
+++ app/models/card.rb
@@ -6,4 +6,8 @@ class Card < ActiveRecord::Base
   validates_presence_of :puzzles, :user_id, :abstract_card_id
   validates_numericality_of :puzzles, :only_integer => true, :greater_than_or_equal_to => 0, :less_than => 7
   validates_associated :user, :abstract_card
+  
+  def self.get_cards_by_user(user_id)
+    self.find(:all, :conditions => ['user_id = ?', user_id])
+  end
 end
--- app/models/deck.rb
+++ app/models/deck.rb
@@ -3,4 +3,10 @@ class Deck < ActiveRecord::Base
   
   validates_presence_of :name
   validates_associated :cards
+  validates_size_of :name, :minimum => 1
+  
+  def self.get_decks_by_user(user_id)
+    #TODO: check if method works
+    self.find(:all, :include => :cards, :conditions => ['cards.user_id = ?', user_id])
+  end
 end
