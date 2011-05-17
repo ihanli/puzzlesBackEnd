4b1b725b3087f275ddaa76149764679614d5fba0
Ismail Hanli <ihanli.mmt-b2009@fh-salzburg.ac.at>
2011-05-17 16:55:56 +0200

    moved code from controller to model and added before filter

--- app/controllers/fighters_controller.rb
+++ app/controllers/fighters_controller.rb
@@ -1,33 +1,16 @@
 class FightersController < ApplicationController
   def create
     @fighter = Fighter.new(params[:fighter])
-      
+
     if @fighter.save
       redirect_to battle_path(@fighter.battle_id)
     else
       render :status => 500
     end
   end
-  
+
   def update
     fighters = Fighter.get_fighters_by_battle(params[:battle_id])
-
-    fighters.each do |fighter|
-      tmp_state = fighter.state
-
-      case params[:event]
-      when "proceed" || "wait"
-        #TODO: give id of logged in user as parameter
-        if fighter.user.fb_id == params[:user_id]
-          fighter.wait!
-        else
-          fighter.proceed!
-        end
-      when "finish"
-        fighter.finish!
-      end
-      
-      render :status => 418 unless tmp_state != fighter.state
-    end
+    render :status => 418 unless fighters.transition_to(params[:event])
   end
 end
