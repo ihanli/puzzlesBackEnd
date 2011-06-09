d6126af0b372ef362d6fe1f905edc58ba5bd4fe6
Ismail Hanli <ihanli.mmt-b2009@fh-salzburg.ac.at>
2011-06-09 02:20:16 +0200

    converts fb id to integer in create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(params[:user])
+    user = User.new(params[:user].to_i)
     if user.save
       redirect_to user_path(user.id)
     else
