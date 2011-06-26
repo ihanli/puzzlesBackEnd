10e0a01f2ee2d1ea50031fd2fd896358ad01bd04
Ismail Hanli <ihanli.mmt-b2009@fh-salzburg.ac.at>
2011-06-26 15:47:36 +0200

    fixed nil object bug (hopefully)

--- app/controllers/battles_controller.rb
+++ app/controllers/battles_controller.rb
@@ -20,6 +20,7 @@ class BattlesController < ApplicationController
   def create
     battle = Battle.new(:rid => params[:rid])
     if battle.save
+      battle = Battle.find_by_rid(params[:rid])
       user = User.find_by_fb_id(params[:fbid])
       fighter = Fighter.new(:user_id => user.id, :battle_id => battle.id, :deck_id => Deck.find_decks_by_user(user.id).first)
       head 500 unless fighter.save
