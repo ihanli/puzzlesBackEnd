d6ecb6149418cf3505fbb0d6be36c94b381f872d
Ismail Hanli <ihanli.mmt-b2009@fh-salzburg.ac.at>
2011-06-29 22:28:56 +0200

    bug fixed update action...no, really

--- app/controllers/card_in_games_controller.rb
+++ app/controllers/card_in_games_controller.rb
@@ -1,21 +1,21 @@
 class CardInGamesController < ApplicationController
   def update
-    @card = CardInGame.find_by_id(params[:id])
+    card = CardInGame.find_by_id(params[:id])
 
     if params[:event]
-      if @card.transition_to(params[:event])
+      if card.transition_to(params[:event])
         head 200
       else
         head 418
       end
     elsif params[:target]
-      if @card.update_attributes(:target_id => params[:target])
+      if card.update_attribute(:target_id, params[:target])
         head 200
       else
         head 500
       end
     else
-      @card.use_talent
+      card.use_talent
       
       respond_to do |format|
         format.xml
